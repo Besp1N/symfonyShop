f9a5401dd4b21628bc73dc03999f4815238c5e81
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-03-09 21:44:24 +0100

    added shirt view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductsRepository $productsRepository): Response
     {
-        return $this->render('home/index.html.twig', []);
+        return $this->render('home/index.html.twig', [
+            "products" => $productsRepository->findBy(["isDisplayOnly" => true])
+        ]);
+    }
+
+    #[Route('/{product}', name: 'app_home_show_one')]
+    public function showOne(Products $product): Response
+    {
+        return $this->render('home/product.html.twig', [
+            'product' => $product
+        ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Cart;
+use App\Entity\Products;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,27 +19,32 @@ class AppFixtures extends Fixture
 
     public function load(EntityManagerInterface|ObjectManager $manager): void
     {
-        $user = new User();
-        $user->setName("Kacper");
-        $user->setLastname("Karabinowski");
-        $user->setEmail("kackar87@wp.pl");
-        $user->setPassword($this->userPasswordHasher->hashPassword($user, "12345678"));
-        $user->setRoles(['ROLE_USER']);
-        $user->setIsActive(true);
-        $user->setActivationKey("test");
+        $user1 = new User();
+        $user1->setName("Kacper");
+        $user1->setLastname("Karabinowski");
+        $user1->setEmail("kackar87@wp.pl");
+        $user1->setPassword($this->userPasswordHasher->hashPassword($user1, "12345678"));
+        $user1->setRoles(['ROLE_USER']);
+        $user1->setIsActive(true);
+        $user1->setActivationKey("test");
+
+        $user1Cart = new Cart();
+        $user1Cart->setTotal(0.00);
+        $user1->setCart($user1Cart);
+
+        $product1 = new Products();
+        $product1->setName("Nike Shirt");
+        $product1->setSize("XL");
+        $product1->setPrice(120.00);
+        $product1->setBrand("Nike");
+        $product1->setDescription("Cool shirt");
+        $product1->setImage("images/products/nike_shirt1.jpg");
+        $product1->setIsDisplayOnly(true);
+        $product1->setType("shirt");
 
 
-        $user2 = new User();
-        $user2->setName("Kacper");
-        $user2->setLastname("Karabinowski");
-        $user2->setEmail("test@wp.pl");
-        $user2->setPassword($this->userPasswordHasher->hashPassword($user2, "fuckoff"));
-        $user2->setRoles(['ROLE_USER']);
-        $user2->setIsActive(true);
-        $user2->setActivationKey("test");
-
-        $manager->persist($user2);
-        $manager->persist($user);
+        $manager->persist($user1);
+        $manager->persist($product1);
         $manager->flush();
 
 
