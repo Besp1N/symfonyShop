b43fce3e9c05386413c94e8248f7f846f7d7cd26
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-03-11 22:29:50 +0100

    added flash in eventListener

--- src/EventListener/CheckActiveUserListener.php
+++ src/EventListener/CheckActiveUserListener.php
@@ -2,17 +2,22 @@
 
 namespace App\EventListener;
 
-use App\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Exception\DisabledException;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\SecurityEvents;
 
 class CheckActiveUserListener implements EventSubscriberInterface
 {
-    public static function getSubscribedEvents()
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public static function getSubscribedEvents(): array
     {
         return [
             SecurityEvents::INTERACTIVE_LOGIN => 'onInteractiveLogin'
@@ -22,8 +27,10 @@ class CheckActiveUserListener implements EventSubscriberInterface
     public function onInteractiveLogin(InteractiveLoginEvent $event): void
     {
         $user = $event->getAuthenticationToken()->getUser();
+        $request = $this->requestStack->getCurrentRequest();
 
         if (!$user->isIsActive()) {
+            $request->getSession()->getFlashBag()->add('warning', 'Twoje konto jest nieaktywne.');
             throw new DisabledException('Twoje konto jest nieaktywne.');
         }
     }
